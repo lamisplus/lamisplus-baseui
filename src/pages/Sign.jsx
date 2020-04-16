dd077c11e02a422e9f4adee0ee9fe183e1695db2
mathew <Adegbite>
2020-04-16 22:07:59 +0100

    modification to the sign page

--- src/pages/Sign.jsx
+++ src/pages/Sign.jsx
@@ -1,17 +1,18 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory  } from "react-router-dom";
+// import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
-
+import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import sigInLogo from 'assets/img/signin.jpg';
 import logo200Image from 'assets/img/logo/logo_200.png';
 
 function Copyright() {
@@ -27,10 +28,20 @@ function Copyright() {
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: `url(${sigInLogo})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
   paper: {
-    marginTop: theme.spacing(8),
+    margin: theme.spacing(8, 4),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
@@ -48,9 +59,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignIn() {
-  const classes = useStyles();
+export default function SignInSide() {
   let history = useHistory();
+  const classes = useStyles();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
@@ -84,28 +95,25 @@ export default function SignIn() {
       isButtonDisabled || handleLogin();
     }
   };
+
   return (
-    <div style={{  
-      //backgroundImage: `url(${sigInLogo})`,
-      backgroundColor: '#fff',
-      backgroundPosition: 'center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'repeat',
-      height: "100%"
-    }}>
-    <Container component="main" maxWidth="xs" >
+    <Grid container component="main" className={classes.root}>
       <CssBaseline />
-      <div className={classes.paper} >
-      <img
+      <Grid item xs={false} sm={4} md={9} className={classes.image} />
+      <Grid item xs={12} sm={8} md={3} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          
+            <img
               src={logo200Image}
               className="rounded"
               style={{cursor: 'pointer' }}
               alt="logo"              
             />
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
+          <br/>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate>
             <TextField
               error={error}
               variant="outlined"
@@ -160,9 +168,8 @@ export default function SignIn() {
               <Copyright />
             </Box>
           </form>
-      </div>
-     
-    </Container>
-    </div>
+        </div>
+      </Grid>
+    </Grid>
   );
 }
