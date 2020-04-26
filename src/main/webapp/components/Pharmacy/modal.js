a2bed552450e9226732d2d55d5a54995035bafaf
Jephtah <uchejephtah@gmail.com>
2020-04-26 21:05:35 +0100

    merging

--- src/main/webapp/components/Pharmacy/modal.js
+++ src/main/webapp/components/Pharmacy/modal.js
@@ -1,100 +1,262 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from 'react';
+import {  Modal, ModalHeader, ModalBody,
+Form,
+Row,
+Col,Input,
+FormGroup,
+Label,Card, CardBody
+} from 'reactstrap';
+import MatButton from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import SaveIcon from '@material-ui/icons/Save'
+import CancelIcon from '@material-ui/icons/Cancel'
+import { connect } from 'react-redux';
+import Chip from '@material-ui/core/Chip';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "react-widgets/dist/css/react-widgets.css";
+import { DateTimePicker } from 'react-widgets';
+import Moment from 'moment';
+import momentLocalizer from 'react-widgets-moment';
+import moment from "moment";
+import {url} from '../../../api'
+import { Spinner } from 'reactstrap';
+import { useSelector, useDispatch } from 'react-redux';
+import { Alert } from 'reactstrap';
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSave = this.handleSave.bind(this);
-    this.state = {
-      title: "",
-      msg: "",
-    };
-  }
+Moment.locale('en');
+momentLocalizer();
+const useStyles = makeStyles(theme => ({
+  card: {
+    margin: theme.spacing(20),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  },
+  cardBottom: {
+    marginBottom: 20
+  },
+  Select: {
+    height: 45,
+    width: 350
+  },
+  button: {
+    margin: theme.spacing(1)
+  },
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      title: nextProps.title,
-      msg: nextProps.msg,
-    });
+  root: {
+    '& > *': {
+      margin: theme.spacing(1)
+    }
+  },
+  input: {
+    display: 'none'
   }
+}))
 
-  titleHandler(e) {
-    this.setState({ title: e.target.value });
-  }
+const ModalSample = (props) => {
+  const classes = useStyles()
+  const [newdata, setNewdata] = useState({formdata});
+  const [loading, setLoading] = useState(false)
+/* Fetch from from the store after clicking the collect sample when the modal triger it will fetch from the store */
+  const formdata = useSelector(state => state.laboratory.formdata);
+  const dispatch = useDispatch();
+  const lab_id = props.datasample.id
+  console.log(props.datasample)
+  const labId = lab_id;
 
-  msgHandler(e) {
-    this.setState({ msg: e.target.value });
-  }
+  useEffect(() => {
+    dispatch(fetchFormById(labId));
+    setNewdata({...newdata, formdata}) 
+  }, [labId]);
+        console.log(formdata.data) 
 
-  handleSave() {
-    const item = this.state;
-    this.props.saveModalDetails(item);
-  }
+        const [data, setData] = useState({data:{}, encounterId:""})
+ 
+          
+        const [optionsample, setOptionsample] = useState([]);
+        useEffect(() => {
+            async function getCharacters() {
+              try {
+                const response = await fetch(url+'application-codesets/codesetGroup?codesetGroup=SAMPLE_TYPE');
+                const body = await response.json();
+                setOptionsample(body.map(({ display, id }) => ({ title: display, value: id })));
+              } catch (error) {
+                console.log(error);
+              }
+            }
+            getCharacters();
+          }, []);
+       const handleInputChangeSample = e => {
+        const { name, value } = e.target
+        const fieldValue = { [name]: value }
+        setSamples({
+            ...samples,
+            ...fieldValue
+        })
 
-  render() {
-    return (
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog" role="document">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Edit Jewel
-              </h5>
-              <button
-                type="button"
-                className="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div className="modal-body">
-              <p>
-                <span className="modal-lable">Title:</span>
-                <input
-                  value={this.state.title}
-                  onChange={(e) => this.titleHandler(e)}
-                />
-              </p>
-              <p>
-                <span className="modal-lable">Msg:</span>
-                <input
-                  value={this.state.msg}
-                  onChange={(e) => this.msgHandler(e)}
-                />
-              </p>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-dismiss="modal"
-              >
-                Close
-              </button>
-              <button
-                type="button"
-                className="btn btn-primary"
-                data-dismiss="modal"
-                onClick={() => {
-                  this.handleSave();
-                }}
-              >
-                Save changes
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    }
+    const saveSample = e => {
+      e.preventDefault()
+      const onSuccess = () => {
+        setLoading(false);
+        // props.history.push("/collect-sample")        
+      }
+      const onError = () => {
+        setLoading(false);        
+      }
+
+    }
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <ToastContainer autoClose={3000} hideProgressBar />
+          <Modal
+            isOpen={props.modalstatus}
+            toggle={props.togglestatus}
+            className={props.className}
+            size="lg"
+          >
+            <Form onSubmit={saveSample}>
+              <ModalHeader toggle={props.togglestatus}>
+                Dispense Drugs
+              </ModalHeader>
+              <ModalBody>
+                <Card>
+                  <CardBody>
+                    <Row>
+                      <Col md={12}>
+                        <Alert
+                          color="dark"
+                          style={{
+                            backgroundColor: "#9F9FA5",
+                            color: "#000",
+                            fontWeight: "bolder",
+                          }}
+                        >
+                          <p style={{ marginTop: ".7rem" }}>
+                            Drug Prescribed:{" "}
+                            <span style={{ fontWeight: "bolder" }}>
+                              {Paracetamol}
+                            </span>
+                            &nbsp;&nbsp;&nbsp; Quantity Prescribed :
+                            <span style={{ fontWeight: "bolder" }}>{20}</span>
+                            &nbsp;&nbsp;&nbsp; Stock Balance :
+                            <span style={{ fontWeight: "bolder" }}>{500}</span>
+                          </p>
+                        </Alert>
+                      </Col>
+                      <Col md={6}>
+                        <FormGroup>
+                          <Label for="maritalStatus">Date Collected</Label>
+
+                          <DateTimePicker
+                            time={false}
+                            name="date_sample_collected"
+                            id="date_sample_collected"
+                            value={samples.date_sample_collected}
+                            onChange={(value1) =>
+                              setSamples({
+                                ...samples,
+                                date_sample_collected: value1,
+                              })
+                            }
+                            defaultValue={new Date()}
+                            max={new Date()}
+                            required
+                          />
+                        </FormGroup>
+                      </Col>
+                      <Col md={6}>
+                        <FormGroup>
+                          <Label for="maritalStatus">Sample Type</Label>
+                          <Autocomplete
+                            multiple
+                            id="sample_type"
+                            size="small"
+                            options={optionsample}
+                            getOptionLabel={(option) => option.title}
+                            onChange={(e, i) =>
+                              setSamples({ ...samples, sample_type: i })
+                            }
+                            renderTags={(value, getTagProps) =>
+                              value.map((option, index) => (
+                                <Chip
+                                  label={option.title}
+                                  {...getTagProps({ index })}
+                                  disabled={index === 0}
+                                />
+                              ))
+                            }
+                            style={{ width: "auto", marginTop: "-1rem" }}
+                            s
+                            renderInput={(params) => (
+                              <TextField
+                                {...params}
+                                variant="outlined"
+                                margin="normal"
+                              />
+                            )}
+                          />
+                          {/* <FixedTags onChange={handleInputChangeSample} value={samples.sample_type} /> */}
+                        </FormGroup>
+                      </Col>
+
+                      <Col md="12">
+                        <FormGroup>
+                          <Label for="maritalStatus">Note</Label>
+                          <Input
+                            type="textarea"
+                            name="comment"
+                            id="comment"
+                            onChange={handleInputChangeSample}
+                            value={samples.comment}
+                          ></Input>
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <br />
+                    {loading ? <Spinner /> : ""}
+                    <br />
+                    <MatButton
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      className={classes.button}
+                      startIcon={<SaveIcon />}
+                      disabled={loading}
+                    >
+                      Ok
+                    </MatButton>
+
+                    <MatButton
+                      variant="contained"
+                      color="default"
+                      onClick={props.togglestatus}
+                      className={classes.button}
+                      startIcon={<CancelIcon />}
+                    >
+                      Cancel
+                    </MatButton>
+                  </CardBody>
+                </Card>
+              </ModalBody>
+            </Form>
+          </Modal>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
-export default Modal;
+export default connect(null, { createCollectedSample, fetchFormById })(ModalSample);
