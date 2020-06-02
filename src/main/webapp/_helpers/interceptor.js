98cb191b4dab28fbd886f39b72095d69b0755e6b
Brian Mwasi <>
2020-06-02 15:55:04 +0300

    Added a todo

--- src/main/webapp/_helpers/interceptor.js
+++ src/main/webapp/_helpers/interceptor.js
@@ -21,6 +21,7 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
     return response;
   }, function (error) {
+      // TODO: Brian Update 403(Forbiden) once roles is completed 
     if (error.response.status === 401 || error.response.status == 403) {
         // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
         authentication.logout();
